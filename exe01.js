10cc5c4e70fa4558e94f63ca439bdc1c0dc7b343
hannahgoes <hannahgoes@hotmail.com>
2022-06-05 23:40:14 -0300

    Sincronismo e Assincronismo

---
+++ exe01.js
@@ -0,0 +1,13 @@
+function obrigada (){
+    console.log("Obrigada!")
+}
+
+function deNada(){
+    console.log("De nada!")
+}
+
+obrigada()
+
+//A ordem que as funções forem chamadas elas serão executadas
+
+deNada();
